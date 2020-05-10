8095e776e356306c7019f8015766b50da57a1e5d
garyli@yeahka.com <lqazyv587>
2020-05-10 20:33:11 +0800

    添加链表相交问题

--- src/com/sky/algorithm/interview/simple/linkedlist/FindFirstIntersectNode.java
+++ src/com/sky/algorithm/interview/simple/linkedlist/FindFirstIntersectNode.java
@@ -1,8 +1,6 @@
 package com.sky.algorithm.interview.simple.linkedlist;
 
 
-import com.sky.algorithm.interview.simple.undo.Code_14_FindFirstIntersectNode;
-
 /**
  * 两个单链表相交的一系列问题
  * 【题目】 在本题中，单链表可能有环，也可能无环。给定两个 单链表的头节点 head1和head2，这两个链表可能相交，也可能 不相交。
@@ -29,7 +27,7 @@ public class FindFirstIntersectNode {
         Node loopNode = getLoopNode(head1);
         System.out.println(loopNode);
     }
-    
+
     public static Node getFirstIntersect(Node head1, Node head2) {
         if (head1 == null || head2 == null) {
             return null;
@@ -43,12 +41,63 @@ public class FindFirstIntersectNode {
         }
         // 两条链表都存在环， 环链表相交
         if (loop1 != null && loop2 != null) {
-
+            return loop(loop1, loop2, head1, head2);
         }
         //普通链表与环链表， 不相交
         return null;
     }
 
+    /**
+     * 找到有环链表相交
+     *
+     * @param head1
+     * @param head2
+     * @return
+     */
+    private static Node loop(Node loop1, Node loop2, Node head1, Node head2) {
+        Node cur1 = null;
+        Node cur2 = null;
+        if (loop1 == loop2) {//其实就是无环链表的查找相交节点的逻辑
+            cur1 = head1;
+            cur2 = head2;
+
+            int n = 0;
+            while (cur1.next != loop1) {//一直循环loop1
+                n++;
+                cur1 = cur1.next;
+            }
+            while (cur2.next != loop1) {//一直循环loop2
+                n--;
+                cur2 = cur2.next;
+            }
+
+            //此时cur1 cur2是loop节点。 对于有环节点来说， 一定是cur1=cur2
+            cur1 = n > 0 ? head1 : head2;//长链表头
+            cur2 = cur1 == head1 ? head2 : head1;//短链表头
+
+            n = Math.abs(n);
+            while (n != 0) {
+                cur1 = cur1.next;
+                n--;
+            }
+
+            while (cur1 != cur2) {
+                cur1 = cur1.next;
+                cur2 = cur2.next;
+            }
+            return cur1;
+        } else {
+            cur1 = loop1;
+
+            while (cur1 != null) {
+                if (cur1 == loop2) {
+                    return cur1;
+                }
+                cur1 = cur1.next;
+            }
+            return null;
+        }
+    }
 
 
     /**
@@ -86,12 +135,13 @@ public class FindFirstIntersectNode {
 
     /**
      * 获取两个无环节点的第一个相交节点
+     *
      * @param head1
      * @param head2
      * @return
      */
     private static Node noLoop(Node head1, Node head2) {
-        if (head1 ==null || head2 == null) {
+        if (head1 == null || head2 == null) {
             return null;
         }
 
@@ -103,7 +153,6 @@ public class FindFirstIntersectNode {
             n++;
             cur1 = cur1.next;
         }
-
         while (cur2.next != null) {
             n--;
             cur2 = cur2.next;
