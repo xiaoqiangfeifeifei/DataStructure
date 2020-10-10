95f58e92b49bac83748f04fdca9979b5e1425fd2
garyli@yeahka.com <lqazyv587>
2020-10-10 22:10:18 +0800

    同意提交

--- src/com/sky/Main.java
+++ src/com/sky/Main.java
@@ -1,11 +1,17 @@
 package com.sky;
 
-import java.util.Arrays;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
 
     public static void main(String[] args) {
-        String a[] = "503da029-0cbd-4223-9c4a-ae04f6aaf61d".split("/");
-        System.out.println(Arrays.toString(a));
+        //...
+        ReentrantLock lock = new ReentrantLock();
+        try {
+            lock.lock();
+            //...
+        } finally {
+            lock.unlock();
+        }
     }
 }
